a1237c45d7f178244aca2dc26949c0c09d263bd1
Angela7758 <54589853+Angela7758@users.noreply.github.com>
2026-02-08 15:16:32 +0100

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -129,6 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# WhiteNoise configuration
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 CORS_ALLOW_ALL_ORIGINS = True
 
